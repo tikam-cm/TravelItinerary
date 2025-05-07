37f940830a0efd7b7ce711ac1d60f7ebad53d43f
Tikam Chand Mahawar <tikamchand.mahawar@gmail.com>
2025-05-07 13:01:20 +0530

    UPDATE: settings for vercel

--- app.js
+++ app.js
@@ -49,8 +49,4 @@ app.use(function(req, res, next) {
 // error handler
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
-
 module.exports = app;
