80b72e9de67ae94d87ea39e3713dfcef306bbf8b
Tikam Chand Mahawar <tikamchand.mahawar@gmail.com>
2025-05-07 13:19:34 +0530

    UPDATE: mailer config to seperate file

---
+++ config/nodeMailer.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILER_HOST,
+  port: process.env.MAILER_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAILER_USER, // generated ethereal user
+    pass: process.env.MAILER_PASS, // generated ethereal password
+  },
+});
+
+module.exports = transporter;
--- utilities/mailerUtility.js
+++ utilities/mailerUtility.js
@@ -1,19 +1,4 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
-
-// Create a test account or replace with real credentials.
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILER_HOST,
-  port: process.env.MAILER_PORT,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.MAILER_USER, // generated ethereal user
-    pass: process.env.MAILER_PASS, // generated ethereal password
-  },
-});
-
-// Wrap in an async IIFE so we can use await.
-
+const transporter = require("../config/nodeMailer.config");
 
 const sendMailNotification = async (email, itineraryData) => {
     (async () => {
